89b0841b24abacb2e2f226724d9cfab9fa263508
Emano <emano.waldeck@gmail.com>
2018-04-04 15:56:18 +0430

    open-in-vlc

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 /* globals exports */
 'use strict';
 
-exports.id = 'com.eccorem.node';
-exports.version = '0.1.0';
+exports.id = 'emano.waldeck';
+exports.version = '0.1.3';
 
 exports.ids = {
-  chrome: [],
-  firefox: []
+  chrome: [
+    'cjjiafgjjkoonchbncbebpghoojakbgm',  // play-in-vlc (chrome)
+    'enlgmhpfbiaifddipblnamdjenddgiih',  // play-in-vlc (opera)
+  ],
+  firefox: [
+    '{ff83c451-2c38-4ae9-baf0-7181faad4ee8}', // play-in-vlc
+  ]
 };
