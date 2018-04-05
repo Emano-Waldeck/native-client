6ff4365c1abad78abcaa570cdad663ff0f38a5f1
Emano <emano.waldeck@gmail.com>
2018-04-05 18:59:30 +0430

    open-in-kmplayer

--- config.js
+++ config.js
@@ -2,14 +2,16 @@
 'use strict';
 
 exports.id = 'emano.waldeck';
-exports.version = '0.1.3';
+exports.version = '0.1.4';
 
 exports.ids = {
   chrome: [
     'cjjiafgjjkoonchbncbebpghoojakbgm',  // play-in-vlc (chrome)
     'enlgmhpfbiaifddipblnamdjenddgiih',  // play-in-vlc (opera)
+    'idnmjmlfpkomkdfhcdmndlnodilkgfbk',  // play-in-vlc (chrome)
   ],
   firefox: [
     '{ff83c451-2c38-4ae9-baf0-7181faad4ee8}', // play-in-vlc
+    '{16c3afcb-bef7-4dd7-8773-1c59b99bee86}', // play-in-kmplayer
   ]
 };
