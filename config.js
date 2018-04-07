08c83c7b499946edfb9cfb325825985571ec7d36
Emano <emano.waldeck@gmail.com>
2018-04-07 19:55:34 +0430

    download-by-idm

--- config.js
+++ config.js
@@ -2,16 +2,18 @@
 'use strict';
 
 exports.id = 'emano.waldeck';
-exports.version = '0.1.4';
+exports.version = '0.1.5';
 
 exports.ids = {
   chrome: [
     'cjjiafgjjkoonchbncbebpghoojakbgm',  // play-in-vlc (chrome)
     'enlgmhpfbiaifddipblnamdjenddgiih',  // play-in-vlc (opera)
     'idnmjmlfpkomkdfhcdmndlnodilkgfbk',  // play-in-vlc (chrome)
+    'lgbipmmmnjifkiiikaffhceflifbmhib',  // download-by-idm (chrome)
   ],
   firefox: [
     '{ff83c451-2c38-4ae9-baf0-7181faad4ee8}', // play-in-vlc
     '{16c3afcb-bef7-4dd7-8773-1c59b99bee86}', // play-in-kmplayer
+    '{165e553c-8c91-4d66-8f47-1e60295562d3}', // download-by-idm
   ]
 };
