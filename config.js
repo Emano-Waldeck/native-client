28f7122357735b32302c84d86589abbf8f47fa22
Emano <emano.waldeck@gmail.com>
2018-04-09 16:08:45 +0430

    play-in-potplayer

--- config.js
+++ config.js
@@ -2,19 +2,21 @@
 'use strict';
 
 exports.id = 'emano.waldeck';
-exports.version = '0.1.6';
+exports.version = '0.1.7';
 
 exports.ids = {
   chrome: [
     'cjjiafgjjkoonchbncbebpghoojakbgm',  // play-in-vlc (chrome)
     'enlgmhpfbiaifddipblnamdjenddgiih',  // play-in-vlc (opera)
     'idnmjmlfpkomkdfhcdmndlnodilkgfbk',  // play-in-vlc (chrome)
+    'alhflifmgbhjpbmjeliahgaoleoahoej',  // play-in-potplayer (chrome)
     'lgbipmmmnjifkiiikaffhceflifbmhib',  // download-by-idm (chrome)
     'kbeogbbnlbfcpdnfbcaljdbbipfdaokh',  // download-by-fdm (chrome)
   ],
   firefox: [
     '{ff83c451-2c38-4ae9-baf0-7181faad4ee8}', // play-in-vlc
     '{16c3afcb-bef7-4dd7-8773-1c59b99bee86}', // play-in-kmplayer
+    '{0b5e9347-1495-4916-ba44-f3b367827c3f}', // play-in-potplayer
     '{165e553c-8c91-4d66-8f47-1e60295562d3}', // download-by-idm
     '{f58cf83a-497b-4f4b-a8ee-13d70fad1fe5}', // download-by-fdm
   ]
